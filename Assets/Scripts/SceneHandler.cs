efaf098b6c5e60b5e289785840a9565d11b0be7a
Deleye Arthur <arthurdeleye@gmail.com>
2021-09-03 20:46:58 +0200

    fdgdjujfuhl 2

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using NaughtyAttributes;
+
+public class SceneHandler : MonoBehaviour
+{
+    [Header("Parameter"), Scene]
+    public int scene;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/Visual/RunAnim.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAnim : MonoBehaviour
+{
+    [Header("Parameter")]
+    public Quaternion rightRot;
+    public Quaternion leftRot;
+    [Range(0,0.5f)]
+    public float cadence = 0.2f;
+    public bool pause;
+
+    //
+    private float timer = 0f;
+    private bool leftFoot;
+
+    void Start()
+    {
+        timer = cadence;
+    }
+    private void Update()
+    {
+        if (!pause)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
+            {
+                timer = cadence;
+                Move();
+            }
+        }
+    }
+
+    public void Move()
+    {
+        if (leftFoot)
+        {
+            transform.rotation = leftRot;
+        }
+        else
+        {
+            transform.rotation = rightRot;
+        }
+
+        leftFoot = !leftFoot;
+    }
+    public void SetPause(bool pauseState)
+    {
+        pause = pauseState;
+    }
+
+}
